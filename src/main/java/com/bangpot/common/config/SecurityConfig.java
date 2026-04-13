4b53592082296ea252a969779558ae8cbf04b40c
taesik <gksrmf359@gmail.com>
2026-04-13 14:27:35 +0900

    fix: cors delete 허용 추가

--- src/main/java/com/bangpot/common/config/SecurityConfig.java
+++ src/main/java/com/bangpot/common/config/SecurityConfig.java
@@ -145,7 +145,7 @@ public class SecurityConfig {
 	CorsConfigurationSource corsConfigurationSource(AuthFrontendProperties authFrontendProperties) {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of(authFrontendProperties.getBaseUrl()));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "OPTIONS"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
 		configuration.setExposedHeaders(List.of("Set-Cookie"));
