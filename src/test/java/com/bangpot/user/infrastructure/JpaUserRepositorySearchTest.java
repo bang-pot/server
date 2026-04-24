3ec27fbd9032344a698a829d79cbb02c0cfac61e
taesik <gksrmf359@gmail.com>
2026-04-24 22:09:23 +0900

    fix: CI PostgreSQL 테스트 환경 명시

--- src/test/java/com/bangpot/user/infrastructure/JpaUserRepositorySearchTest.java
+++ src/test/java/com/bangpot/user/infrastructure/JpaUserRepositorySearchTest.java
@@ -7,18 +7,14 @@ import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.bangpot.user.application.port.UserQueryRepository;
 import com.bangpot.user.domain.view.UserSearchView;
 
 @DataJpaTest
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(JpaUserQueryRepository.class)
 class JpaUserRepositorySearchTest {
 
