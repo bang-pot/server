08b924afb4e2778a7884633f43091b8dbe26219b
taesik <gksrmf359@gmail.com>
2026-04-24 22:01:19 +0900

    fix: CI 테스트 설정 보강

--- src/test/java/com/bangpot/user/infrastructure/JpaUserRepositoryTest.java
+++ src/test/java/com/bangpot/user/infrastructure/JpaUserRepositoryTest.java
@@ -7,13 +7,17 @@ import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.bangpot.user.application.port.UserRepository;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(JpaUserRepository.class)
 class JpaUserRepositoryTest {
 
