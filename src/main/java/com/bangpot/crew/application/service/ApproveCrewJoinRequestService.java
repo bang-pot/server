ab5960177f7f47bc04aa6616a7b7c9c9031c9700
taesik <gksrmf359@gmail.com>
2026-04-26 20:57:07 +0900

    refactor: 가입 승인 멤버십 복구 로직 정리

--- src/main/java/com/bangpot/crew/application/service/ApproveCrewJoinRequestService.java
+++ src/main/java/com/bangpot/crew/application/service/ApproveCrewJoinRequestService.java
@@ -4,6 +4,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import com.bangpot.crew.application.exception.CrewJoinRequestNotFoundException;
 import com.bangpot.crew.application.exception.CrewNotFoundException;
 import com.bangpot.crew.application.port.CrewJoinRequestRepository;
@@ -36,16 +38,21 @@ public class ApproveCrewJoinRequestService implements ApproveCrewJoinRequestUseC
 			command.crewId()
 		).orElseThrow(() -> new CrewJoinRequestNotFoundException(command.crewId(), command.requestId()));
 
-		if (!crewMemberRepository.existsByCrewIdAndUserId(command.crewId(), joinRequest.getUserId())) {
-			var existingMembership = crewMemberRepository.findAnyByCrewIdAndUserId(command.crewId(), joinRequest.getUserId());
-			if (existingMembership.isPresent()) {
-				CrewMember crewMember = existingMembership.get();
+		Optional<CrewMember> existingMembership = crewMemberRepository.findAnyByCrewIdAndUserId(
+			command.crewId(),
+			joinRequest.getUserId()
+		);
+
+		if (existingMembership.isPresent()) {
+			CrewMember crewMember = existingMembership.get();
+			if (!crewMember.isActive()) {
 				crewMember.reactivateAsMember();
 				crewMemberRepository.save(crewMember);
-			} else {
-				crewMemberRepository.save(CrewMember.createMember(command.crewId(), joinRequest.getUserId()));
 			}
+		} else {
+			crewMemberRepository.save(CrewMember.createMember(command.crewId(), joinRequest.getUserId()));
 		}
+
 		joinRequest.approve();
 		crewJoinRequestRepository.save(joinRequest);
 
